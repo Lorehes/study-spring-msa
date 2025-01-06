158ab623aa2882987d83b6901bcd60370e4cbc27
Lorehes <lorehes4892@gmail.com>
2025-01-06 16:50:38 +0900

    first

---
+++ src/main/java/microserver_demo/micorserver_demo/MicorserverDemoApplication.java
@@ -0,0 +1,13 @@
+package microserver_demo.micorserver_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicorserverDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicorserverDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/microserver_demo/micorserver_demo/MicorserverDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package microserver_demo.micorserver_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicorserverDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
